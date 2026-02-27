2671b670e64091c5b7a2d88fe1417a950e6623ee
krys_leung <krystalleung88@gmail.com>
2026-02-27 17:30:30 -0500

    chore: get query for proper element

--- element.sh
+++ element.sh
@@ -26,6 +26,5 @@ else
 fi
 
 # query for info based on atomic number
-
-
+echo $($PSQL "select e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius from elements as e left join properties as p using (atomic_number) left join types as t using (type_id) where e.atomic_number=$ATOMIC")
 
