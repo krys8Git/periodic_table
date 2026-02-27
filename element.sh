40fa55417bbc8bc67dbaf6aa5897db6a6ce5fcb3
krys_leung <krystalleung88@gmail.com>
2026-02-27 17:28:15 -0500

    chore: adding if statements to assign atomic number

--- element.sh
+++ element.sh
@@ -1,3 +1,31 @@
 #! /bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 echo "Please provide an element as an argument."
+
+if [[ -z $(echo "$1" | grep ^[0-9]+$) ]]
+then
+  ATOM1=$($PSQL "select atomic_number from elements where symbol='$1'")
+  ATOM2=$($PSQL "select atomic_number from elements where name='$1'")
+else
+  ATOM3=$($PSQL "select atomic_number from elements where atomic_number=$1")
+fi
+
+# assign atomic number to atomic variable
+if [[ -n $ATOM1 ]]
+then
+  ATOMIC=$ATOM1
+elif [[ -n $ATOM2 ]]
+then
+  ATOMIC=$ATOM2
+elif [[ -n $ATOM3 ]]
+then
+  ATOMIC=$ATOM3
+else
+  echo "I could not find that element in the database"
+fi
+
+# query for info based on atomic number
+
+
+
