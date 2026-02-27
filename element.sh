813f73fcd4e459b403abcde3be541f944d45d30f
krys_leung <krystalleung88@gmail.com>
2026-02-27 17:37:59 -0500

    fix: order of code

--- element.sh
+++ element.sh
@@ -3,6 +3,16 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 echo "Please provide an element as an argument."
 
+# helper function to get attributes of element
+GET_ATTRIBUTES() {
+  # query for info based on atomic number
+  ATTRIBUTES=$($PSQL "select e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius from elements as e left join properties as p using (atomic_number) left join types as t using (type_id) where e.atomic_number=$1")
+  echo "$ATTRIBUTES" | while IFS='|' read AN NAME SYMBOL TYPE AM MP BP
+  do
+    echo "$BP"
+  done
+}
+
 if [[ -z $(echo "$1" | grep ^[0-9]+$) ]]
 then
   ATOM1=$($PSQL "select atomic_number from elements where symbol='$1'")
@@ -14,17 +24,14 @@ fi
 # assign atomic number to atomic variable
 if [[ -n $ATOM1 ]]
 then
-  ATOMIC=$ATOM1
+  GET_ATTRIBUTES $ATOM1
 elif [[ -n $ATOM2 ]]
 then
-  ATOMIC=$ATOM2
+  GET_ATTRIBUTES $ATOM2
 elif [[ -n $ATOM3 ]]
 then
-  ATOMIC=$ATOM3
+  GET_ATTRIBUTES $ATOM3
 else
-  echo "I could not find that element in the database"
+  echo "I could not find that element in the database."
 fi
-
-# query for info based on atomic number
-echo $($PSQL "select e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius from elements as e left join properties as p using (atomic_number) left join types as t using (type_id) where e.atomic_number=$ATOMIC")
 
